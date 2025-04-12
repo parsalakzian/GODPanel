be2095c1c3aeac0f9a4750414c1d132bc80fe6b1
Parsa Lakzian <parsalakzian@gmail.com>
2025-04-12 15:24:43 +0330

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -3,6 +3,19 @@
 set -e
 
 PROJECT_NAME="GODPanel"
+GITHUB_REPO="https://github.com/parsalakzian/GODPanel.git"
 INSTALL_DIR="/root/$PROJECT_NAME"
+PYTHON_VERSION="3.12.8"
+ENV_NAME="$PROJECT_NAME-env"
+ADMIN_FILE="$INSTALL_DIR/admin.json"
+SERVICE_FILE="/etc/systemd/system/$PROJECT_NAME.service"
 
-sudo rm -r "$INSTALL_DIR/static"
+
+
+# توقف سرویس اگر وجود دارد
+if systemctl list-units --full -all | grep -q "$PROJECT_NAME.service"; then
+    echo "Stopping existing service..."
+    systemctl stop "$PROJECT_NAME.service"
+fi
+
+sudo rm -r "$INSTALL_DIR"
